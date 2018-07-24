31cba031e16889a4bada7ca6353ef026890e3234
Andreas Umbach <umbach@gmail.com>
2018-07-24 20:29:36 +0200

    - support for commands - descriptions can be turned on/off - templates and exercises are now in separate folders

--- mktex.pl
+++ mktex.pl
@@ -2,8 +2,7 @@
 
 use strict;
 
-sub readDat {
-	my $infile = shift;
+sub readDat { my $infile = shift;
 	open(my $ifh, $infile) || die "can't open $infile";
 
 	my %map = ();
@@ -11,6 +10,7 @@ sub readDat {
 	my %dat = ( "map" => \%map, "list" => \@list );
 	while(<$ifh>)
 	{
+		next if(/^#/);
 		chomp();
 		if(/(\w+)=(.+)/)
 		{
@@ -49,15 +49,30 @@ my %sheet = readDat($infile);
 my @problems = ();
 for my $problem (@{$sheet{"list"}})
 {
-	my %p = readDat("Aufgaben/" . $problem . ".dat");
-	push @problems, \%p;
+	if($problem =~ /^%%(\w+)%%(.*)/)
+	{
+		my %p = ( "command" => $1, "param" => $2 );		
+		push @problems, \%p;
+	}
+	else
+	{
+		my %p = readDat("Aufgaben/" . $problem . ".dat");
+		push @problems, \%p;
+	}
 }
 
 # %sheet contains the meta data
 my %maphash = %{$sheet{"map"}};
 # @problems contains each problem
 
-open(my $tfh, "template.tex") || die "no document template";
+my $nodesc = 0;
+
+my $template = "template.tex";
+if(defined $maphash{"Template"})
+{
+	$template = $maphash{"Template"};
+}
+open(my $tfh, "Templates/$template") || die "no document template";
 while(<$tfh>)
 {
 	while(/%%(\w+)%%/g)
@@ -68,39 +83,69 @@ while(<$tfh>)
 			for my $pref (@problems)
 			{
 				my %phash = %{$pref};
-				my %pmhash = %{$phash{"map"}};
-				my @plist = @{$phash{"list"}};
-
-				open(my $pfh, "problem.tex") || die "no problem template";
-				while(<$pfh>)
+				if(defined $phash{"command"})
 				{
-					while(/%%(\w+)%%/g)
+					my $cmd = $phash{"command"};
+					if($cmd eq "pagebreak")
 					{
-						my $token = $1;
-						if($token eq "Tasks")
-						{
-							# print tasks
-							for my $task (@plist)
-							{
-								print "\\task $task\n";
-							}
-							$_ = "";
-						}
-						else
-						{
-							s/%%$token%%/$pmhash{$token}/;
-						}
+						print "\\newpage\n";
 					}
-					print;
+					elsif($cmd eq "desc")
+					{
+						print $phash{"param"}. "\n\n";
+					}
+					elsif($cmd eq "nodesc")
+					{
+						$nodesc = 1;
+					}
 				}
-				close($pfh);
+				else
+				{
+					my %pmhash = %{$phash{"map"}};
+					my @plist = @{$phash{"list"}};
+
+					open(my $pfh, "Templates/problem.tex") || die "no problem template";
+					while(<$pfh>)
+					{
+						while(/%%(\w+)%%/g)
+						{
+							my $token = $1;
+							if($token eq "Tasks")
+							{
+								# print tasks
+								for my $task (@plist)
+								{
+									print "\\task $task\n";
+								}
+								$_ = "";
+							}
+							else
+							{
+								s/%%$token%%/$pmhash{$token}/;
+								if($token eq "Description" && $nodesc == 1)
+								{
+									$_ = "\$ \$\n";
+								}
+							}
+						}
+						print;
+					}
+					close($pfh);
+				}
 			}		
 			$_ = "";
 		}
 		else
 		{
 			# print("replacing $token with " . $maphash{$token} . "\n");
-			s/%%$token%%/$maphash{$token}/;
+			if(defined $maphash{$token})
+			{
+				s/%%$token%%/$maphash{$token}/;
+			}
+			else
+			{
+				$_ = "";
+			}
 		}
 	}
 	print;
